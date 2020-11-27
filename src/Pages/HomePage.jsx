d4a2e5d9434dc7d5ed4e0701703f3ddcb502a41e
Asmovic <asogba007@gmail.com>
2020-11-27 18:19:00 +0100

    Initial Commit

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-tabs */
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable react/button-has-type */
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+
+class HomePage extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        Welcome Home!!!
+      </div>
+    );
+  }
+}
+
+export default HomePage;
